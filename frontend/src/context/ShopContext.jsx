b96af98d8e919b8f069e11b17d21616353a2ddc7
Adarsh Kumar Singh <145589188+Adarsh0710@users.noreply.github.com>
2025-07-09 12:21:02 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider = (props) => {
 
     const currency = 'Rs. ';
     const delivery_fee = 50;
-    const backendUrl = 'https://trunkandtuskbackend.vercel.app/'
+    const backendUrl = 'https://my-ecommerce-8yer.onrender.com'
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
